6a25cc54751b3505e37220ae7c7318123d0933ab
yoshiswagg <aldolouati2@gmail.com>
2026-08-09 19:02:18 +0200

    Refactor speedhack implementation and feature list

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -19,6 +19,9 @@
 // ========== TARGET LIBRARY ==========
 #define targetLibName OBFUSCATE("libil2cpp.so")
 
+// ========== GLOBALS ==========
+uintptr_t il2cppBase = 0;
+
 // ========== TOGGLES ==========
 bool ticketToggle = true;
 bool hasBanTimeToggle = false;
@@ -106,12 +109,11 @@ float get_VisionMultiplier(void* instance) {
     return old_get_VisionMultiplier(instance) * visionMultiplierValue;
 }
 
-// Speedhack - set timeScale
-void (*set_timeScale)(float speed);
+// Speedhack - set timeScale using il2cppBase + offset
+typedef void (*SetTimeScale_t)(float value);
 void setGameSpeed(float speed) {
-    if (set_timeScale) {
-        set_timeScale(speed);
-    }
+    auto setTimeScale = (SetTimeScale_t)(il2cppBase + 0x30E6FE4);
+    setTimeScale(speed);
 }
 
 // ========== MENU FEATURES ==========
@@ -127,7 +129,7 @@ jobjectArray GetFeatureList(JNIEnv *env, jobject context) {
             OBFUSCATE("307_CollapseAdd_Toggle_Auto Complete Tasks"),
             OBFUSCATE("308_CollapseAdd_Toggle_Easy Report"),
             OBFUSCATE("309_CollapseAdd_SeekBar_Vision Multiplier_1_15"),
-            OBFUSCATE("310_CollapseAdd_SeekBar_Speedhack_1_10"),
+            OBFUSCATE("200_CollapseAdd_SeekBar_Speedhack_1_10"),
     };
 
     int Total_Feature = (sizeof features / sizeof features[0]);
@@ -171,8 +173,8 @@ void Changes(JNIEnv *env, jclass clazz, jobject obj, jint featNum, jstring featN
         case 309:
             visionMultiplierValue = (float)value;
             break;
-        case 310:
-            setGameSpeed((float)value);
+        case 200:
+            setGameSpeed(value);  // Removed (float) cast
             break;
         default:
             break;
@@ -185,10 +187,10 @@ void hack_thread() {
         sleep(1);
     }
 
-#if defined(__aarch64__)
-    // Get timeScale setter from UnityEngine.CoreModule
-    set_timeScale = (void (*)(float))getAbsoluteAddress(targetLibName, OBFUSCATE("0x306EF4"));
+    il2cppBase = getLibraryBase(targetLibName);
+    LOGI(OBFUSCATE("il2cppBase: 0x%lx"), il2cppBase);
 
+#if defined(__aarch64__)
     HOOK(targetLibName, "0x2758374", AddOption, old_AddOption);
     HOOK(targetLibName, "0x2A90704", HasBanTime, old_HasBanTime);
     HOOK(targetLibName, "0x2FC7434", HasAssetsToDownload, old_HasAssetsToDownload);
